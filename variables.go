c23d23051281ba98d2b2ddd2bce7a4a127226c75
Shahidnsu <shahid.khan@northsouth.edu>
2022-10-30 23:35:06 +0600

    learned about git variables

---
+++ variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+
+	fmt.Println(b, c)
+
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+
+	f := "apple" // := sytanx is shortand for declaring and initializing a variable, e.g for var  f string ="apple" in this case
+	fmt.Println(f)
+
+}

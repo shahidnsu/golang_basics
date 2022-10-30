0ac562271ab57f37fa546050a11ef24b26f0bb61
Shahidnsu <shahid.khan@northsouth.edu>
2022-10-30 23:53:13 +0600

    learned about ifelse in go

---
+++ ifelse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+
+	} else {
+		fmt.Println("7 is odd")
+	}
+}
+
+//else needs come after closing brackets in go
